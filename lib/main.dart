3c21ac9133acf674422e8e373bec2a0811c9c218
BraulioUcPech <braulioalexson@gmail.com>
2023-11-03 09:44:12 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<List<Album>> futureAlbums;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbums = fetchAlbums();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Album>>(
+            future: futureAlbums,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    final album = snapshot.data?[index];
+                    return ListTile(
+                      title: Text(album!.title),
+                      subtitle:
+                          Text('User ID: ${album.userId}, ID: ${album.id}'),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // Por defecto, muestra un indicador de carga.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Future<List<Album>> fetchAlbums() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+
+  if (response.statusCode == 200) {
+    Iterable data = jsonDecode(response.body);
+    List<Album> albums = data.map((json) => Album.fromJson(json)).toList();
+    return albums;
+  } else {
+    throw Exception('Failed to load albums');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
